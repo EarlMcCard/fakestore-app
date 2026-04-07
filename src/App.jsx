fa1b004408280b04be4298e208b4358f374f82e4
Earl McCard <earlmccard.dev@gmail.com>
2026-04-07 12:42:44 -0400

    Updated Filter bar and Search bar

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     const fetchProducts = async () => {
       try {
         const response = await axios.get('https://fakestoreapi.com/products')
+        console.log(response.data)
         setProducts(response.data)
       } catch (err) {
         setError('Failed to fetch products. Please try again later.')
@@ -19,18 +20,37 @@ function App() {
     fetchProducts()
   }, [])
 
-  const [filterCategory, setFilterCategory] = useState("All")
-  const displayedProducts = filterCategory === "All" ? products : products.filter(product => product.category === filterCategory)
-  
+const [filterCategory, setFilterCategory] = useState("All")
+    
+const [searchTerm, setSearchTerm] = useState("")
+
+const displayedProducts = products.filter((product) => {
+    const matchesCategory = filterCategory === "All" || product.category === filterCategory;
+    const matchesSearch = product?.name?.toLowerCase().includes(searchTerm?.toLowerCase());
+    return matchesCategory && matchesSearch;
+  });
+
   const handleFilterChange = (category) => {
     setFilterCategory(category)
   }
 
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value)
+  }
 
   return (
     <div className='app-container'>
       <header>
         <h1>React FakeStore</h1>
+
+          <div className="search-bar">
+            <input type="text" 
+            placeholder='Search for products...'
+            value={searchTerm}
+            onChange={handleSearchChange}
+            />
+          </div>
+
          <div className="filter-buttons">
           <button onClick={() => handleFilterChange("All")}>All</button>
           <button onClick={() => handleFilterChange("electronics")}>Electronics</button>
@@ -41,6 +61,7 @@ function App() {
       </header>
       <main>
         <ProductList products={displayedProducts} />
+        
       </main>
     </div>
   )
